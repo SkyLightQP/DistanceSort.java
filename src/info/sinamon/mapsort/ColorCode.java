70cb85dd3ff68e4c3add3140038441c8ad7860e2
combbm <combbm@naver.com>
2020-11-13 00:04:22 +0900

    feat: create ColorCode.java to display console with color

---
+++ src/info/sinamon/mapsort/ColorCode.java
@@ -0,0 +1,8 @@
+package info.sinamon.mapsort;
+
+public class ColorCode {
+    public static String GREEN = "\033[0;32m";
+    public static String RED = "\033[0;31m";
+    public static String YELLOW = "\033[0;33m";
+    public static String RESET = "\033[0m";
+}
